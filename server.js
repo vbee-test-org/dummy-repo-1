f4f8c73a2ef38634ff1c69d226611d0ddc811716
Long Ly <longhienly112@gmail.com>
2024-03-31 23:02:26 +0700

    ...

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.use(cors())
 app.use("/api/articles", articlesRoutes)
 dotenv.config()
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 const connectionSTring = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionSTring, { dbName: "WebData" })
     .then(() => {
         console.log("Connected successfully to MongoDB");
-        app.listen(port, "localhost", () => {console.log(`Listening on port ${port}`)});
+        app.listen(port, () => {console.log(`Listening on port ${port}`)});
     })
     .catch(err => console.log(err));
 
