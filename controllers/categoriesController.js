37ddd63cedfd6410cac1fc561521966d3d3c37cd
Long Ly <longhienly112@gmail.com>
2024-05-14 16:54:34 +0700

    Fixing bugs

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -24,7 +24,7 @@ const getArticleCategories = async (req, res) => {
         path: "articles",
         options: {
           sort: { creation_date: -1 },
-          perDocumentLimit: 30,
+          perDocumentLimit: 10,
           populate: {
             path: "publisher",
           }
@@ -62,15 +62,14 @@ const getArticleCategories = async (req, res) => {
 /***********************************Search article categories****************************************/
 const searchArticleCategories = async (req, res) => {
   // Get params
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 30;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 30;
   const text = req.query.text;
   const opt = req.query.opt || "e";
 
   if (!text) {
     return res.status(400).json({ error: "text must not be empty!" })
   }
-
   // Redis instance
   const redis = new Redis(process.env.REDIS_URL);
   const searchCache = await redis.get(`articles_${text}_${opt}_${page}`);
@@ -82,22 +81,27 @@ const searchArticleCategories = async (req, res) => {
   }
   // Cache miss
   try {
-    var results;
+    var searchTerm, physicalPage, results, count;
     if (opt === "r") {
-      results = await ArticleCategory.find({ category: { $regex: `^${text}` } })
+      searchTerm = {$regex: `^${text}`}
+      physicalPage = null;
+      results = await ArticleCategory.find({ category: searchTerm })
         .populate({
           path: "articles",
           options: {
             sort: { creation_date: -1 },
-            perDocumentLimit: 30,
+            perDocumentLimit: 10,
             populate: {
               path: "publisher"
             }
           }
         });
+      count = results.length*10;
     }
     else if (opt === "e") {
-      results = await ArticleCategory.find({ category: { $eq: text } })
+      searchTerm = {$eq: text};
+      physicalPage = page;
+      results = await ArticleCategory.find({ category: searchTerm })
         .populate({
           path: "articles",
           options: {
@@ -109,36 +113,36 @@ const searchArticleCategories = async (req, res) => {
             limit: limit
           }
         });
+      const articlesCount = await ArticleCategory.aggregate([
+        {$match: {category: searchTerm}},
+        {$project: {_id: 0, count: { $size: "$articles_guid"}}}
+      ]);
+      count = articlesCount[0].count;
     }
     else {
       return res.status(400).json({ error: "Undefined search term" })
     }
-    const count = results.reduce((acc, current) => {
-      acc + current.articles_count
-    }, 0);
-    const categories = {
-      categories: results.map(category => ({
-        category: category.category,
-        articles: category.articles.map(article => ({
-          article_link: article.article_link,
-          article_title: article.article_title,
-          author: article.author,
-          creation_date: article.creation_date,
-          thumbnail_image: article.thumbnail_image,
-          article_summary: article.article_summary,
-          article_detailed_content: article.article_detailed_content,
-          categories: article.categories,
-          publisher: {
-            name: article.publisher?.[0]?.name || "",
-            logo: article.publisher?.[0]?.logo || ""
-          }
-        }))
+    const categories = results.map(category => ({
+      category: category.category,
+      articles: category.articles.map(article => ({
+        article_link: article.article_link,
+        article_title: article.article_title,
+        author: article.author,
+        creation_date: article.creation_date,
+        thumbnail_image: article.thumbnail_image,
+        article_summary: article.article_summary,
+        article_detailed_content: article.article_detailed_content,
+        categories: article.categories,
+        publisher: {
+          name: article.publisher?.[0]?.name || "",
+          logo: article.publisher?.[0]?.logo || ""
+        }
       }))
-    }
+    }));
 
-    redis.set(`articles_${text}_${opt}_${page}`, JSON.stringify({ count, totalPages: Math.ceil(count / limit), currentPage: page, categories }), "EX", 600);
+    redis.set(`articles_${text}_${opt}_${physicalPage}`, JSON.stringify({ count, totalPages: opt === "r"? 1: Math.ceil(count / limit), currentPage: physicalPage, categories }), "EX", 600);
     redis.quit();
-    res.status(200).json({ count, totalPages: Math.ceil(count / limit), currentPage: page, categories });
+    res.status(200).json({ count, totalPages: opt === "r"? 1: Math.ceil(count / limit), currentPage: page, categories });
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
@@ -198,52 +202,78 @@ const getPostCategories = async (req, res) => {
 
 /***********************************Search post categories****************************************/
 const searchPostCategories = async (req, res) => {
+  // Get params
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 30;
   const text = req.query.text;
   const opt = req.query.opt || "e";
 
   if (!text) {
     return res.status(400).json({ error: "text must not be empty!" })
   }
-
-  var searchTerm;
-  if (opt === "r") {
-    searchTerm = { $regex: `^${text}` };
+  // Redis instance
+  const redis = new Redis(process.env.REDIS_URL);
+  const searchCache = await redis.get(`posts_${text}_${opt}_${page}`);
+  // Cache hit
+  if (searchCache) {
+    console.log("Fetching results from cache");
+    redis.quit();
+    return res.status(200).json(JSON.parse(searchCache));
   }
-  else if (opt === "e") {
-    searchTerm = { $eq: text };
-  }
-  else {
-    return res.status(400).json({ error: "Undefined search term" })
-  }
-
+  // Cache miss
   try {
-    const categories = await PostCategory.find({ category: searchTerm }).populate({
-      path: "posts",
-      options: {
-        sort: { creation_date: -1 }
-      }
-    });
-    const count = categories.length;
-    const reduced_categories = {
-      count,
-      categories: categories.map(category => ({
-        category: category.category,
-        posts: category.posts.map(post => ({
-          post_link: post.post_link,
-          website_source: post.website_source,
-          post_title: post.post_title,
-          author: post.author,
-          creation_date: post.creation_date,
-          post_content: post.post_content,
-          categories: post.categories,
-          up_votes: post.up_votes,
-          down_votes: post.down_votes,
-          media_url: post.media_url
-        }))
-      }))
+    var searchTerm, physicalPage, results, count;
+    if (opt === "r") {
+      searchTerm = {$regex: `^${text}`}
+      physicalPage = null;
+      results = await PostCategory.find({ category: searchTerm }).populate({
+        path: "posts",
+        options: {
+          sort: { creation_date: -1 },
+          perDocumentLimit: 10,
+        }
+      });
+      count = results.length*10;
     }
+    else if (opt === "e") {
+      searchTerm = {$eq: text};
+      physicalPage = page;
+      results = await PostCategory.find({ category: searchTerm }).populate({
+        path: "posts",
+        options: {
+          sort: { creation_date: -1 },
+          skip: (page - 1) * limit,
+          limit: limit
+        }
+      });
+      const postsCount = await PostCategory.aggregate([
+        {$match: {category: searchTerm}},
+        {$project: {_id: 0, count: { $size: "$posts_guid"}}}
+      ]);
+      count = postsCount[0].count;
+    }
+    else {
+      return res.status(400).json({ error: "Undefined search term" })
+    }
+    const categories = results.map(category => ({
+      category: category.category,
+      posts: category.posts.map(post => ({
+        post_link: post.post_link,
+        website_source: post.website_source,
+        post_title: post.post_title,
+        author: post.author,
+        creation_date: post.creation_date,
+        post_content: post.post_content,
+        categories: post.categories,
+        up_votes: post.up_votes,
+        down_votes: post.down_votes,
+        media_url: post.media_url
+      }))
+    }));
 
-    res.status(200).json(reduced_categories);
+    redis.set(`posts_${text}_${opt}_${page}`, JSON.stringify({count, totalPages: opt === "r"? 1 : Math.ceil(count / limit), currentPage: physicalPage, categories}), "EX", 600);
+    redis.quit();
+    res.status(200).json({count, totalPages: opt === "r"? 1 : Math.ceil(count / limit), currentPage: physicalPage, categories});
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
